02c5dafd913a52b04c3f4d1938d70bf4077e9f91
Axelle LNU <aaposramennoodles@gmail.com>
2026-02-17 07:58:14 -0500

    Added genshin stats as well

--- update_stats.py
+++ update_stats.py
@@ -66,6 +66,77 @@ async def fetch_memory_of_chaos(client, uid):
     except Exception as e:
         print("Failed to fetch Memory of Chaos:", e)
         return {}
+    
+async def fetch_hsr_data(client, uid):
+    try:
+        user = await client.get_starrail_user(uid)
+
+        # Fetch characters
+        character_response = await client.get_starrail_characters(uid)
+        characters = character_response.avatar_list
+
+        # Filter 5-star characters
+        five_stars = [
+            char.name for char in characters if char.rarity == 5
+        ]
+
+        hsr_notes = await client.get_starrail_notes(uid=uid)
+        moc_data = await fetch_memory_of_chaos(client, uid)
+
+        return {
+            "nickname": user.info.nickname,
+            "server": user.info.server,
+            "level": user.info.level,
+            "avatar": "data/avatar.png",
+            "avatar_url": user.info.avatar,
+            "achievements": user.stats.achievement_num,
+            "active_days": user.stats.active_days,
+            "avatar_count": user.stats.avatar_num,
+            "chest_count": user.stats.chest_num,
+            "five_star_characters": five_stars,
+
+            "stamina": hsr_notes.current_stamina,
+            "current_train_score": hsr_notes.current_train_score,
+
+            "memory_of_chaos": moc_data,
+        }
+    except Exception as e:
+        print("Failed to fetch HSR data:", e)
+        return {}
+    
+async def fetch_genshin_data(client, uid):
+    try:
+        user = await client.get_genshin_user(uid)
+        characters = await client.get_genshin_characters(uid)
+
+        five_stars = [
+            char.name for char in characters if char.rarity == 5
+        ]
+
+        notes = await client.get_genshin_notes(uid)
+
+        oculus = user.stats.anemoculi + user.stats.geoculi + user.stats.electroculi + user.stats.dendroculi + user.stats.hydroculi + user.stats.pyroculi + user.stats.lunoculi
+
+        chests = user.stats.common_chests + user.stats.exquisite_chests + user.stats.precious_chests + user.stats.luxurious_chests + user.stats.remarkable_chests
+
+        return {
+            "nickname": user.info.nickname,
+            "server": user.info.server,
+            "level": user.info.level,
+            "achievements": user.stats.achievements,
+            "active_days": user.stats.days_active,
+            "character_count": user.stats.characters,
+            "oculus": oculus,
+            "chest_count": chests,
+            "five_star_characters": five_stars,
+
+            "resin": notes.current_resin,
+            "daily_task": notes.daily_task.completed_tasks
+        }
+    
+    except Exception as e:
+        print("Failed to fetch Genshin data:", e)
+        return {}
 
 
 async def main():
@@ -79,46 +150,21 @@ async def main():
     client = genshin.Client(cookies)
 
     hsr_uid = int(os.environ["HOYOLAB_HSR_UID"])
+    genshin_uid = int(os.environ["HOYOLAB_GENSHIN_UID"])
 
-    # Fetch full Star Rail profile
-    user = await client.get_starrail_user(hsr_uid)
-
-    # Fetch characters
-    character_response = await client.get_starrail_characters(hsr_uid)
-    characters = character_response.avatar_list
-
-    # Filter 5-star characters
-    five_stars = [
-        char.name for char in characters if char.rarity == 5
-    ]
-
-    hsr_notes = await client.get_starrail_notes(uid=hsr_uid)
-    moc_data = await fetch_memory_of_chaos(client, hsr_uid)
+    # fetch both games
+    hsr_data = await fetch_hsr_data(client, hsr_uid)
+    genshin_data = await fetch_genshin_data(client, genshin_uid)
 
     data = {
         "last_updated": datetime.utcnow().isoformat(),
-        "genshin": {},
-        "hsr": {
-            "nickname": user.info.nickname,
-            "server": user.info.server,
-            "level": user.info.level,
-            "avatar": "data/avatar.png",
-            "achievements": user.stats.achievement_num,
-            "active_days": user.stats.active_days,
-            "avatar_count": user.stats.avatar_num,
-            "chest_count": user.stats.chest_num,
-            "five_star_characters": five_stars,
-
-            "stamina": hsr_notes.current_stamina,
-            "current_train_score": hsr_notes.current_train_score,
-
-            "memory_of_chaos": moc_data,
-        },
+        "genshin": genshin_data,
+        "hsr": hsr_data,
     }
 
     os.makedirs("data", exist_ok=True)
 
-    avatar_url = user.info.avatar
+    avatar_url = hsr_data.get("avatar_url", "")
     avatar_path = "data/avatar.png"
 
     try:
